f693a165a3de6bbb8d5ee9f0f4a18830f0e921a1
G384242023 <238424@st.takushoku-u.ac.jp>
2024-07-04 16:25:27 +0900

    第5回

--- kazuate.js
+++ kazuate.js
@@ -4,25 +4,22 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 1;
-
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei);
 
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let i = document.querySelector('input[name="kaitou"]');
+  let yoso = Number(i.value);
   let span = document.querySelector('span#kaisu');
   span.textContent = kaisu;
   span = document.querySelector('span#answer');
-  console.log(kaisu+"回目の予想:"+yoso);
+  span.textContent = yoso;
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
